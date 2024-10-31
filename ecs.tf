c076ee6e656d1b9d5cfa76c3501eedbc8ed21249
IshiharaTakashi <ishihara.takashi@ryobi.co.jp>
2024-10-31 17:29:48 +0900

    commit11

--- ecs.tf
+++ ecs.tf
@@ -1,8 +1,19 @@
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = local.app
+}
+
+# ecs.tfのaws_cloudwatch_log_groupリソースをリネーム
+resource "aws_cloudwatch_log_group" "cloudwatch_log_group_ecs" {
+  name              = "/ecs/${local.app}"
+  retention_in_days = 7
+}
+
+
 resource "aws_ecs_task_definition" "ecs_task_definition" {
   family             = local.app
   execution_role_arn = aws_iam_role.ecs.arn
   task_role_arn      = aws_iam_role.ecs_task.arn
-  network_mode       = "bridge"  # EC2モードに適した設定
+  network_mode       = "bridge"
 
   container_definitions = <<CONTAINERS
 [
@@ -54,6 +65,44 @@ resource "aws_ecs_service" "ecs_service" {
   depends_on = [aws_lb_listener_rule.alb_listener_rule]
 }
 
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = local.app
+resource "aws_instance" "ecs_instance" {
+  ami                    = "<ECS-optimized AMI ID>"  # リージョンに応じた ECS 最適化 AMI を使用
+  instance_type          = "t2.micro"
+  iam_instance_profile   = aws_iam_instance_profile.ecs_instance_profile.name
+  user_data              = <<-EOF
+                            #!/bin/bash
+                            echo ECS_CLUSTER=${aws_ecs_cluster.ecs_cluster.name} >> /etc/ecs/ecs.config
+EOF
+  vpc_security_group_ids = [aws_security_group.ecs_sg.id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Name = "ecs-instance"
+  }
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecs_instance_profile"
+  role = aws_iam_role.ecs_instance_role.name
+}
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecs_instance_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_policy_attach" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
